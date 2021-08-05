1ff6d80cda94a5cd9daa8234c2a2b89c926221b2
Alena DENIS <basstet067@yahoo.com>
2021-08-05 16:49:19 +0200

    feat(script.js): guess the number game

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+// игровой бот "Загадывание случайного числа от 1 до 100"
+
+const isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+const guessNum = function() {
+  const randNum = Math.round(Math.random() * 100);
+  let userNum = prompt(`Угадай число от 1 до 100`);
+
+  const checkNum = function() {
+    if (userNum === null) {
+      alert(`Игра окончена`);
+    } else if (!isNumber(userNum)) {
+      userNum = prompt(`Введи число!`);
+      checkNum();
+    } else {
+      userNum *= 1;
+
+      switch (true) {
+        case userNum === randNum:
+          alert(`Поздравляю, Вы угадали!!!`);
+          break;
+        case userNum > randNum:
+          alert(`Загаданное число меньше`);
+          userNum = prompt(`Попробуй ещё раз:`);
+          checkNum();
+          break;
+        case userNum < randNum:
+          alert(`Загаданное число больше`);
+          userNum = prompt(`Попробуй ещё раз:`);
+          checkNum();
+          break;
+      }
+    }
+  };
+  checkNum();
+};
+guessNum();
